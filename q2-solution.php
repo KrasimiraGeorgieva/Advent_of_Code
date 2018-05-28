1efa40a1346c75736e826ad77e475381baa908e2
Krasimira Georgieva <krassi77bg@gmail.com>
2018-05-29 00:00:16 +0300

    Changed the loop.

--- q2-solution.php
+++ q2-solution.php
@@ -1,20 +1,15 @@
 <?php
 // 2017 Day 5: A Maze of Twisty Trampolines, All Alike
 
-require 'q2-input.txt';
-
 $arr = file("q2-input.txt", FILE_IGNORE_NEW_LINES);
-
-$jumpOffsets = 0;
 $instructionIndex = 0;
 
-do {
+while ($instructionIndex >= 0 && $instructionIndex < count($arr)){
+    static $jumpOffsets = 0;
     $jumpOffsets += 1;
-    $instructionIndex += $arr[$instructionIndex]++;
-	
-} while ($instructionIndex >= 0 && $instructionIndex < count($arr));
+    $currentIndex = $arr[$instructionIndex];
+    $arr[$instructionIndex] += 1;
+    $instructionIndex += $currentIndex;
+}
 
-echo PHP_EOL;
-print_r($arr);
 echo $jumpOffsets . PHP_EOL; // 336905
-echo $instructionIndex;
